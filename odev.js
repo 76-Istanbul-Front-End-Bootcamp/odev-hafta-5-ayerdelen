b52acbea86f134a1b397114efd68448d5a2d01af
Aycan Aleyna Yerdelen <aycanyerdelen@gmail.com>
2021-01-01 18:06:08 +0000

    bitti

--- odev.js
+++ odev.js
@@ -19,7 +19,7 @@ import {createTableElements} from "./main.js";
     ids for buttons and select
 
     Population - bigger than 500.000 => #populationBigger
-    land area - less than 1000 => #landAreaLess
+    land area - less than "1000 => #landAreaLess
     Does any city has population less than 100.000? => #isPopulationLess
     Does every city has land area bigger than 100? => #isLandBigger
     city select => #selectcity
@@ -31,6 +31,55 @@ document.querySelector("#reset").addEventListener("click", () => {
     createTableElements([], "singlecity")
 });
 
+ 
 /* START CODING HERE */
 
+document.querySelector("#populationBigger").addEventListener("click", () => {
+  let popBigger = data.filter(elements => elements.population > 500000)
+   createTableElements(popBigger,"allcities");
+})
 
+document.querySelector("#landAreaLess").addEventListener("click",()=> {
+  let lessThan = data.filter(elements => elements.landArea < 1000)
+   createTableElements(lessThan,"allcities");
+})
+
+document.querySelector("#isPopulationLess").addEventListener("click",() => {
+  let populationSome = data.some(elements => elements.population < 100000)
+  if(populationSome){
+    alert("YES")
+  }
+  else {
+    alert ("NO")
+  }
+})
+
+document.querySelector("#isLandBigger").addEventListener("click",()=> {
+  let landEvery = data.every(elements => elements.landArea > 100)
+  if(landEvery){
+    alert("YES")
+  }
+  else{
+    alert("NO")
+  }
+})
+
+const dataCities = data.map(cities => cities.name);
+  const citySelect =document.querySelector("#inputGroupSelect01")
+  for(let i = 0; i<=citySelect.length; i++){
+  let beforeOptions = document.querySelector("option[value]")
+citySelect.removeChild(beforeOptions)
+  }
+    
+dataCities.forEach((element) => {
+   let cityOption = document.createElement("option");
+    cityOption.setAttribute("value", element);
+    cityOption.innerHTML= element;
+    citySelect.appendChild(cityOption);
+    
+})
+
+citySelect.addEventListener("change",(event) => {
+  let oneName = data.filter(element => event.target.value=== element.name)
+  createTableElements(oneName,"singlecity")
+})
